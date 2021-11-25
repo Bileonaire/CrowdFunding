a2cb7602b31e29b59f97290dd64065385b31bd7a
Bileonaire <kiokoleon@gmail.com>
2021-11-25 12:45:51 +0300

    Add Error Handling

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,25 +1,34 @@
 import React, { useState } from 'react';
 import Layout from "../../components/Layout";
-import { Form, Button, Input } from "semantic-ui-react";
+import { Form, Button, Input, Message } from "semantic-ui-react";
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
 
 function New() {
     const [contribution, setContribution] = useState('');
+    const [errorMessage, setError] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const newCampaign = async (event) => {
         event.preventDefault();
-        const accounts = await web3.eth.getAccounts();
-        await factory.methods.createCampaign(contribution, accounts[0] ).send({
-            from: accounts[0]
-        });
+
+        try {
+            setLoading(true);
+            const accounts = await web3.eth.getAccounts();
+            await factory.methods.createCampaign(contribution, accounts[0])
+                .send({ from: accounts[0] });
+
+        } catch (err) {
+            setError(err.message)
+        }
+        setLoading(false)
     };
 
     return (
         <Layout>
         <div>
             <h3>Open Campaigns</h3>
-            <Form onSubmit={newCampaign}>
+            <Form onSubmit={newCampaign} error={!!errorMessage}>
                 <Form.Field>
                 <label>Minimum Contribution</label>
                 <Input
@@ -29,7 +38,8 @@ function New() {
                     value={contribution}
                     onChange={(e) => setContribution(e.target.value)} />
                 </Form.Field>
-                <Button primary type='submit' >Create!</Button>
+                <Message error header="Ooops!" content={errorMessage} />
+                <Button loading={loading} primary type='submit' >Create!</Button>
             </Form>
         </div>
         </Layout>
